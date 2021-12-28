59c2015fb290ea33ce422ebbef60511e63cab599
Valen <v.lunaklick@gmail.com>
2021-12-28 02:00:30 -0300

    Changed semantic from div to header and article

--- src/contact.js
+++ src/contact.js
@@ -1,47 +1,46 @@
-import img from './images/sushicontact.jpg';
+import img from './images/sushicontact.jpg'
 import img1 from './images/street.jpeg'
 
-export default function(){
-    const mainH = document.getElementById('main-cont');
+export default function () {
+	const mainH = document.getElementById('main-cont')
 
-    let divH = document.createElement('div')
-    divH.classList.add('diH');
+	let divH = document.createElement('header')
+	divH.classList.add('diH')
 
-    let titL = document.createElement('h2');
-    titL.classList.add('titH');
-    titL.innerHTML = 'Contact us'
-    divH.appendChild(titL);
+	let titL = document.createElement('h2')
+	titL.classList.add('titH')
+	titL.innerHTML = 'Contact us'
+	divH.appendChild(titL)
 
-    let imgS = document.createElement('img');
-    imgS.setAttribute('src',img1);
-    imgS.classList.add('imgst')
+	let imgS = document.createElement('img')
+	imgS.setAttribute('src', img1)
+	imgS.classList.add('imgst')
 
-    let divHD = document.createElement('div');
+	let divHD = document.createElement('article')
 
-    let pHD = document.createElement('h4');
-    pHD.textContent =  'Number: 555 5555 5555'
-    pHD.classList.add('pCont')
+	let pHD = document.createElement('h4')
+	pHD.textContent = 'Number: 555 5555 5555'
+	pHD.classList.add('pCont')
 
-    let pHDII = document.createElement('h4');
-    pHDII.textContent =  'Address: Fake 12345'
-    pHDII.classList.add('pCont')
+	let pHDII = document.createElement('h4')
+	pHDII.textContent = 'Address: Fake 12345'
+	pHDII.classList.add('pCont')
 
-    let pHDI = document.createElement('h4');
-    pHDI.textContent =  'Email: best@sushi.com'
-    pHDI.classList.add('pCont')
+	let pHDI = document.createElement('h4')
+	pHDI.textContent = 'Email: best@sushi.com'
+	pHDI.classList.add('pCont')
 
-    let imaH = document.createElement('img');
-    imaH.setAttribute('src', `${img}`);
-    imaH.classList.add('homeIm');
-    
-    divHD.appendChild(imaH);
-    divHD.appendChild(pHD);
-    divHD.appendChild(pHDI)
-    divHD.appendChild(imgS);
-    divHD.appendChild(pHDII);
-    divHD.classList.add('divHD');
+	let imaH = document.createElement('img')
+	imaH.setAttribute('src', `${img}`)
+	imaH.classList.add('homeIm')
 
+	divHD.appendChild(imaH)
+	divHD.appendChild(pHD)
+	divHD.appendChild(pHDI)
+	divHD.appendChild(imgS)
+	divHD.appendChild(pHDII)
+	divHD.classList.add('divHD')
 
-    mainH.appendChild(divH);
-    mainH.appendChild(divHD);
+	mainH.appendChild(divH)
+	mainH.appendChild(divHD)
 }
--- src/home.js
+++ src/home.js
@@ -1,32 +1,31 @@
-import img from './images/homeimg.jpg';
+import img from './images/homeimg.jpg'
 
-export default function(){
-    const mainH = document.getElementById('main-cont');
+export default function () {
+	const mainH = document.getElementById('main-cont')
 
-    let divH = document.createElement('div')
-    divH.classList.add('diH');
+	let divH = document.createElement('header')
+	divH.classList.add('diH')
 
-    let titL = document.createElement('h2');
-    titL.classList.add('titH');
-    titL.innerHTML = 'Home of the sushi'
-    divH.appendChild(titL);
+	let titL = document.createElement('h2')
+	titL.classList.add('titH')
+	titL.innerHTML = 'Home of the sushi'
+	divH.appendChild(titL)
 
+	let divHD = document.createElement('article')
 
-    let divHD = document.createElement('div');
+	let pHD = document.createElement('p')
+	pHD.textContent =
+		'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore deleniti distinctio eum eos dolor dolore, blanditiis aliquam est voluptas suscipit corrupti. Temporibus aut qui, tempore iure accusamus mollitia? Minima, cupiditate! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore deleniti distinctio eum eos dolor dolore, blanditiis aliquam est voluptas suscipit corrupti. Temporibus aut qui, tempore iure accusamus mollitia? Minima, cupiditate! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore deleniti distinctio eum eos dolor dolore, blanditiis aliquam est voluptas suscipit corrupti.'
+	pHD.classList.add('pHome')
 
-    let pHD = document.createElement('p');
-    pHD.textContent =  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore deleniti distinctio eum eos dolor dolore, blanditiis aliquam est voluptas suscipit corrupti. Temporibus aut qui, tempore iure accusamus mollitia? Minima, cupiditate! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore deleniti distinctio eum eos dolor dolore, blanditiis aliquam est voluptas suscipit corrupti. Temporibus aut qui, tempore iure accusamus mollitia? Minima, cupiditate! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore deleniti distinctio eum eos dolor dolore, blanditiis aliquam est voluptas suscipit corrupti.'
-    pHD.classList.add('pHome')
+	let imaH = document.createElement('img')
+	imaH.setAttribute('src', `${img}`)
+	imaH.classList.add('homeIm')
 
-    let imaH = document.createElement('img');
-    imaH.setAttribute('src', `${img}`);
-    imaH.classList.add('homeIm');
-    
-    divHD.appendChild(imaH);
-    divHD.appendChild(pHD);
-    divHD.classList.add('divHD');
+	divHD.appendChild(imaH)
+	divHD.appendChild(pHD)
+	divHD.classList.add('divHD')
 
-
-    mainH.appendChild(divH);
-    mainH.appendChild(divHD);
+	mainH.appendChild(divH)
+	mainH.appendChild(divHD)
 }
